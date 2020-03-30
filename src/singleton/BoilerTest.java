70bc78e4d9cc5620fac7b0ddf17934cc36a4a1cc
Николай <nikolaykochkin91@gmail.com>
2020-03-30 14:25:19 +0300

    Singleton done

---
+++ src/singleton/BoilerTest.java
@@ -0,0 +1,10 @@
+package singleton;
+
+public class BoilerTest {
+    public static void main(String[] args) {
+        ChocolateBoiler boiler = ChocolateBoiler.getInstance();
+        boiler.fill();
+        boiler.boil();
+        boiler.drain();
+    }
+}
---
+++ src/singleton/ChocolateBoiler.java
@@ -0,0 +1,50 @@
+package singleton;
+
+public class ChocolateBoiler {
+    private boolean empty;
+    private boolean boiled;
+    private volatile static ChocolateBoiler instance;
+
+    private ChocolateBoiler() {
+        empty = true;
+        boiled = false;
+    }
+
+    public static ChocolateBoiler getInstance() {
+        if (instance == null) {
+            synchronized (ChocolateBoiler.class) {
+                if (instance == null) {
+                    instance = new ChocolateBoiler();
+                }
+            }
+        }
+        return instance;
+    }
+
+    public void fill() {
+        if (isEmpty()) {
+            empty = false;
+            boiled = false;
+        }
+    }
+
+    public void drain() {
+        if (!isEmpty() && isBoiled()) {
+            empty = true;
+        }
+    }
+
+    public void boil() {
+        if(!isEmpty() && !isBoiled()) {
+            boiled = true;
+        }
+    }
+
+    public boolean isEmpty() {
+        return empty;
+    }
+
+    public boolean isBoiled() {
+        return boiled;
+    }
+}
