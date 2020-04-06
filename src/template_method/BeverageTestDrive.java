55d0a911b5b7ee5feb456b98c63a30e013a8f0fd
Николай <nikolaykochkin91@gmail.com>
2020-04-06 17:18:27 +0300

    Template Method

---
+++ src/template_method/BeverageTestDrive.java
@@ -0,0 +1,14 @@
+package template_method;
+
+public class BeverageTestDrive {
+    public static void main(String[] args) {
+        Tea tea = new Tea();
+        Coffee coffee = new Coffee();
+
+        System.out.println("Making tea...");
+        tea.prepareRecipe();
+
+        System.out.println("Making coffee...");
+        coffee.prepareRecipe();
+    }
+}
