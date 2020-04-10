990019fe5c410b39d9e026623ebc4fdc4bac7c07
Николай <nikolaykochkin91@gmail.com>
2020-04-10 17:49:07 +0300

    Iterator

---
+++ src/iterator/DinerMenuIterator.java
@@ -0,0 +1,35 @@
+package iterator;
+
+import java.util.Iterator;
+
+public class DinerMenuIterator implements Iterator<MenuItem> {
+    MenuItem[] items;
+    int position = 0;
+
+    public DinerMenuIterator(MenuItem[] items) {
+        this.items = items;
+    }
+
+    @Override
+    public MenuItem next() {
+        return items[position++];
+    }
+
+    @Override
+    public boolean hasNext() {
+        return position < items.length && items[position] != null;
+    }
+
+    @Override
+    public void remove() {
+        if (position <= 0) {
+            throw new IllegalStateException("You can't remove an item until you've done at least one next()");
+        }
+        if (items[position - 1] != null) {
+            for (int i = position - 1; i < (items.length - 1); i++) {
+                items[i] = items[i - 1];
+            }
+            items[items.length - 1] = null;
+        }
+    }
+}
